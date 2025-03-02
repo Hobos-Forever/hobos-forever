1c88d2b12ecd1993cc9cbc1b05963822b2335d6a
john <dvirusare@gmail.com>
2025-03-02 22:34:17 +0200

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,6 +3,6 @@ import { mainnet, polygon, optimism, arbitrum, bscTestnet } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "Hobos Forever DApp",
-  projectId: "YOUR_PROJECT_ID", // Replace with your WalletConnect Project ID
+  projectId: "88f8bb315447d2c0da385180bf346909", // Replace with your WalletConnect Project ID
   chains: [mainnet, polygon, optimism, arbitrum, bscTestnet],
 });
